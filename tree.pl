5f4109ae332ae21897922ee0b7c3795e5b237929
Chami <lethomeathman@gmail.com>
2024-10-13 23:21:44 +0300

    adde a few more members in the family tree

--- tree.pl
+++ tree.pl
@@ -8,7 +8,12 @@ parent(brian, karen).
 parent(brian,john).
 parent(delilah, brian).
 parent(delilah,john).
+parent(dave, topisia).
+parent(venessa, topisia).
 
 %rule 
-grandparent(x, y) :- parent(X), parent(Y, Z).
-sibling(x, y)
+grandparent(X, Z):- parent(X, Y), parent(Y, Z).
+sibling(T, V):- parent(S, T), parent(S, V).
+aunt(A, U):- parent(B, U), sibling(B, A).
+cousin(P, Q) :- parent(R, P), parent(S, Q), sibling(R, S).
+
